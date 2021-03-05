21a2225224c41418a17b074edea79caed362ddf9
Nahuel Leiva <nahuel.nleiva@gmail.com>
2021-03-05 16:38:27 -0300

    Agregar archivos de proyecto.

---
+++ models/UserInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace signalRTC.models
+{
+    class UserInfo
+    {
+        public string userName { get; set; }
+        public string connectionId { get; set; }
+    }
+}
